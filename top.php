8a2bcf46b2998d4518c316b2563f35fbae9a48eb
myaaaats <myaaaats@gmail.com>
2018-02-25 23:16:09 +0900

    修正

--- top.php
+++ top.php
@@ -1,52 +1,57 @@
 <?php
- 
+
 session_start();
- 
+
 //ログイン済みかを確認
 if (!isset($_SESSION['USER'])) {
     header('Location: login.php');
     exit;
 }
- 
+
 //ログアウト機能
 if(isset($_POST['logout'])){
-    
+
     $_SESSION = [];
     session_destroy();
- 
+
     header('Location: login.php');
     exit;
 }
- 
+
 ?>
- 
+
 <!DOCTYPE html>
 <html>
 <meta charset="UTF-8">
 <head>
     <title>トップ画面</title>
 </head>
- 
+
 <body>
 <h1>トップ画面</h1>
 <p><?php echo $_SESSION['USER'] ?>さんでログイン中</p>
 <br>
-<form action="form.php" method="post">
+<form action="form.php" method="post" enctype="multipart/form-data">
 <p>
 タイトル　※入力必須：<br><input type="title" name="title" size="40" required>
 </p>
 <p>
-公開日　※入力必須：<br><input type="date" name="date" required>
+表示日時　※入力必須：<br><input type="date" name="date" required>
 </p>
 <p>
-公開期限：<br><input type="date" name="enddate">
+すぐに公開しますか？　※入力必須：<br>
+<input type="radio" name="show_flg" value="1" checked> はい
+<input type="radio" name="show_flg" value="0" > いいえ
+</p>
+<p>
+公開日時：<br><input type="date" name="enddate">
 </p>
 <p>
 本文　※入力必須：<br>
 <textarea name="text" rows="4" cols="40" required></textarea>
 </p>
 <p>
-画像ファイル　※入力必須：<br><input type="file" name="img" required>
+画像ファイル　※入力必須：<br><input type="file" name="img" enctype="multipart/form-data" required>
 </p>
 <p>
 <input type="submit" value="送信"><input type="reset" value="リセット">
@@ -55,6 +60,6 @@ if(isset($_POST['logout'])){
 <form method="post" action="top.php">
     <input type="submit" name="logout" value="ログアウト">
 </form>
- 
+
 </body>
 </html>
